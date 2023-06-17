4b336f5c3fc28489e893f594d7fc5eb19726c081
B0nBun <rgusiakov@gmail.com>
2023-06-17 22:22:31 +0300

    refactor: language selection in menu bar instead of a combo box

--- src/main/java/itmo/app/client/Client.java
+++ src/main/java/itmo/app/client/Client.java
@@ -11,9 +11,14 @@ import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.Arrays;
 import java.util.Enumeration;
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JRadioButtonMenuItem;
 import javax.swing.Popup;
 import javax.swing.PopupFactory;
 import javax.swing.Timer;
@@ -22,33 +27,6 @@ import javax.swing.WindowConstants;
 import javax.swing.plaf.FontUIResource;
 
 public class Client {
-
-    @SuppressWarnings("unused")
-    private static void mesengerTest() throws IOException {
-        SocketAddress addr = new InetSocketAddress("127.0.0.1", 2000);
-        @SuppressWarnings({ "resource" })
-        var messenger = new Messenger(addr);
-        messenger.onCollectionUpdate(collectionUpdate -> {
-            System.out.println("Collection update, listener 1");
-        });
-        messenger.onCollectionUpdate(collectionUpdate -> {
-            System.out.println(
-                "Collection: " + Arrays.toString(collectionUpdate.newCollection.toArray())
-            );
-        });
-        while (true) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException err) {}
-            messenger.sendAndThen(
-                new ClientRequest("login", "password", new RequestBody() {}),
-                response -> {
-                    System.out.println("Client got the response, handling...");
-                    System.out.println("Handled response: " + response.body.toString());
-                }
-            );
-        }
-    }
 
     public static final JFrame frame = new JFrame();
     public static Page currentPage = null;
@@ -66,10 +44,31 @@ public class Client {
             }
         }
 
+        var menuBar = new JMenuBar();
+        JMenu menu = Client.createLanguageMenu();
+        menuBar.add(menu);
+
         Client.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         Client.frame.setSize(700, 500);
+        Client.frame.setJMenuBar(menuBar);
         Client.setPage(new LoginAndRegistrationPage());
         Client.frame.setVisible(true);
+    }
+
+    private static JMenu createLanguageMenu() {
+        var menu = new JMenu("Language");
+        var languageButtonGroup = new ButtonGroup();
+        for (var language : LocaleService.getLanguages()) {
+            var item = new JRadioButtonMenuItem(language.toString());
+            item.addActionListener(_action -> {
+                LocaleService.changeLocale(language.locale);
+            });
+            languageButtonGroup.add(item);
+            menu.add(item);
+        }
+        JMenuItem item = menu.getItem(0);
+        if (item != null) item.setSelected(true);
+        return menu;
     }
 
     public static void setPage(Page page) {
@@ -84,7 +83,7 @@ public class Client {
 
     private static Point getPositionForPopup() {
         var framel = Client.frame.getLocation();
-        return new Point(framel.x + 10, framel.y + 50);
+        return new Point(framel.x + 10, framel.y + 70);
     }
 
     private static Timer currentPopupTimer = null;
@@ -110,5 +109,33 @@ public class Client {
             new Timer(Client.POPUP_TIMING, _action -> popup.hide());
         Client.currentPopupTimer.setRepeats(false);
         Client.currentPopupTimer.start();
+    }
+
+    // TODO: Remove when won't be needed
+    @SuppressWarnings("unused")
+    private static void mesengerTest() throws IOException {
+        SocketAddress addr = new InetSocketAddress("127.0.0.1", 2000);
+        @SuppressWarnings({ "resource" })
+        var messenger = new Messenger(addr);
+        messenger.onCollectionUpdate(collectionUpdate -> {
+            System.out.println("Collection update, listener 1");
+        });
+        messenger.onCollectionUpdate(collectionUpdate -> {
+            System.out.println(
+                "Collection: " + Arrays.toString(collectionUpdate.newCollection.toArray())
+            );
+        });
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException err) {}
+            messenger.sendAndThen(
+                new ClientRequest("login", "password", new RequestBody() {}),
+                response -> {
+                    System.out.println("Client got the response, handling...");
+                    System.out.println("Handled response: " + response.body.toString());
+                }
+            );
+        }
     }
 }
--- src/main/java/itmo/app/client/pages/CollectionPage.java
+++ src/main/java/itmo/app/client/pages/CollectionPage.java
@@ -1,13 +1,11 @@
 package itmo.app.client.pages;
 
 import itmo.app.client.Client;
-import itmo.app.client.LocaleService;
 import itmo.app.client.components.TranslatedLabel;
 import java.awt.Component;
 import java.awt.GridBagLayout;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JPanel;
 
 public class CollectionPage extends JPanel implements Page {
@@ -21,17 +19,6 @@ public class CollectionPage extends JPanel implements Page {
             {
                 var label = new TranslatedLabel("test_string");
                 panel.add(label);
-            }
-            {
-                var languageList = new JComboBox<>(
-                    LocaleService.getLanguages().toArray()
-                );
-                languageList.setSelectedIndex(0);
-                languageList.addActionListener(_action -> {
-                    var selected = (LocaleService.Language) languageList.getSelectedItem();
-                    LocaleService.changeLocale(selected.locale);
-                });
-                panel.add(languageList);
             }
             {
                 var button = new JButton("Sign out");
